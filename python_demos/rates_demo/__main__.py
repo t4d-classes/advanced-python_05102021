2adfe97486bf0fab1e299e656c88d3baf9992104
Eric Greene <eric@t4d.io>
2021-05-10 11:56:07 -0400

    config of python demos

---
+++ python_demos/rates_demo/__main__.py
@@ -0,0 +1,9 @@
+""" main module """
+
+def display_message(msg: str) -> None:
+    """ display a message of hope and love """
+    print(msg)
+
+if __name__ == "__main__":
+
+    display_message("Hello, World!")
