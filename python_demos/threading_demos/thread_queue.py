9c07f0493bc8f10e7d34ec6edd715dda8e3a9180
Eric Greene <eric@t4d.io>
2021-05-11 15:30:06 -0400

    add queue exercise solution

--- python_demos/threading_demos/thread_queue.py
+++ python_demos/threading_demos/thread_queue.py
@@ -6,6 +6,7 @@ import time
 from random import randint
 
 nums: queue.Queue[int] = queue.Queue()
+double_nums: queue.Queue[int] = queue.Queue()
 
 
 def generate_nums(queue_nums: queue.Queue[int]) -> None:
@@ -16,17 +17,29 @@ def generate_nums(queue_nums: queue.Queue[int]) -> None:
         queue_nums.put(num)
         time.sleep(1)
 
-def output_nums(queue_nums: queue.Queue[int]) -> None:
-    """ output numbers """
+def double_the_nums(
+    queue_nums: queue.Queue[int],
+    queue_double_nums: queue.Queue[int]) -> None:
+    """ double numbers """
 
     while True:
         num = queue_nums.get()
+        queue_double_nums.put(num * 2)
+
+def output_nums(queue_double_nums: queue.Queue[int]) -> None:
+    """ output numbers """
+
+    while True:
+        num = queue_double_nums.get()
         print(num)
 
 
 
 generate_nums_thread = threading.Thread(target=generate_nums, args=(nums,))
-output_nums_thread = threading.Thread(target=output_nums, args=(nums,))
+double_the_nums_thread = threading.Thread(
+    target=double_the_nums, args=(nums,double_nums))
+output_nums_thread = threading.Thread(target=output_nums, args=(double_nums,))
 
 generate_nums_thread.start()
+double_the_nums_thread.start()
 output_nums_thread.start()
